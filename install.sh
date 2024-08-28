5b2d8e600eefcb979d639e69a2d7ce18d1c34fc1
geeta <geeta.ganta92@gmail.com>
2024-08-28 17:28:39 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@ then
   echo "please run with root previlages"
   exit 1
 fi
-echo "dnf list installed git"
-
+dnf list installed git
+ if [ $userid -ne 0]
+ then
+    echo "Git is not installed ., install it"
+    dnf install git -y  
+ else
+    echo "no need to install"
+ fi   
