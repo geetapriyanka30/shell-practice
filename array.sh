29409da0ffa1776d0741b42b7ab6115932884017
geeta <geeta.ganta92@gmail.com>
2024-08-28 11:15:30 +0530

    array.sh

--- array.sh
+++ array.sh
@@ -2,11 +2,14 @@
 
 FRUITS=("APPLE" "KIWI" "ORANGE")
 
-echo "First fruit is: ${FRUITS(0)}"
-echo "second fruit is: ${FRUITS(1)}"
-echo "third fruit is: ${FRUITS(2)}"
+echo "First fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
 
-echo "All fruits are: ${FRUITS(@)}"
+echo "All fruits are: ${FRUITS[@]}"
+
+
+
 
 
 
