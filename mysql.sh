8bff758a522d2c126b28eb0f8788afa2fce6e6a1
geeta <geeta.ganta92@gmail.com>
2024-08-29 10:41:49 +0530

    mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,16 @@
+userid=$(id -u)
+
+if[ $userid -ne 0 ]
+then
+   echo "install sql with root privelages"
+   exit 1
+fi
+dnf list installed sql 
+if [ $? -ne 0 ]
+then 
+   echo "install sql"
+   dnf install mysql -y
+else
+   echo "it was already installed"
+fi    
+
